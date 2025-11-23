d2fcade58138c734b8df29cb2aeed107a688c9c9
nkl-bhv <nikolajbuhrakov2@gmail.com>
2025-11-23 22:25:57 +0200

    Remove placeholder console log from lesson next handler

--- src/screens/LessonContentScreen.tsx
+++ src/screens/LessonContentScreen.tsx
@@ -7,7 +7,6 @@ interface LessonContentScreenProps {
 export function LessonContentScreen({ onBack }: LessonContentScreenProps) {
   const handleNext = () => {
     // TODO: wire up navigation for future lesson steps
-    console.log('Next step TODO');
   };
 
   return <LessonStep1 onBack={onBack} onNext={handleNext} />;
