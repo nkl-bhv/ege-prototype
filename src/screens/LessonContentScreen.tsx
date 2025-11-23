cc493f7477b0154306a9a674a1e599ea89f93eff
nkl-bhv <nikolajbuhrakov2@gmail.com>
2025-11-23 16:01:35 +0200

    Add TODO handler for next lesson step navigation

--- src/screens/LessonContentScreen.tsx
+++ src/screens/LessonContentScreen.tsx
@@ -5,6 +5,9 @@ interface LessonContentScreenProps {
 }
 
 export function LessonContentScreen({ onBack }: LessonContentScreenProps) {
-  // TODO: add forward navigation when subsequent steps are available
-  return <LessonStep1 onBack={onBack} onNext={() => {}} />;
+  const handleNext = () => {
+    // TODO: wire up navigation for future lesson steps
+  };
+
+  return <LessonStep1 onBack={onBack} onNext={handleNext} />;
 }
